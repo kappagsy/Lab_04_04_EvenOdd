1d5d8f41b7c09b0f8013de3c919d4c1f1f29d55b
kappagsy <kappagsy@mail.uc.edu>
2022-09-16 23:49:19 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int number = 8;
+
+        if (number % 2 == 0) {
+            System.out.println("The selected number is even.");
+        }
+
+            if (number % 2 > 0) {
+                System.out.println("The selected number is odd.");
+            }
+    }
+}
